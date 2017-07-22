9d3768097e486c823f214e647382dc1c26d438df
jx.xie <tsejx@foxmail.com>
2017-07-22 23:16:20 +0800

    component scroll

---
+++ src/components/scroll/Scroll.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const Scroll = function(target,duration){
+  if (window.__scrollMove) return;
+  const begin = document.documentElement.scrollTop || document.body.scrollTop;
+  const change = target - begin;
+  const start = Date.now();
+  (function fn(){
+      window.__scrollMove = window.requestAnimationFrame(fn);
+      var nowTime = Date.now() - start;
+      if(nowTime >= duration){
+        nowTime = duration;
+        window.cancelAnimationFrame(window.__scrollMove);
+        window.__scrollMove = null;
+      }
+      setScrollTop(change / duration * nowTime + begin);
+    })();
+  function setScrollTop(t){
+    return document.documentElement.scrollTop = document.body.scrollTop = t;
+  }
+}
+
+export default Scroll;
