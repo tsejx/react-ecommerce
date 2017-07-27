73ca40d6c4f8d5f09c084785a5fc11be1d440e28
jx.xie <tsejx@foxmail.com>
2017-07-27 15:11:04 +0800

    Updating entry file and route file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RouteApp from './router/Route'; //路由配置
-
 import 'semantic-ui-css/semantic.css';// semantic-ui 样式
 import 'assets/style/reset.scss';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <RouteApp/>,
